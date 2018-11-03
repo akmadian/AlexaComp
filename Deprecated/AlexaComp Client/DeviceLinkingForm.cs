b938eb5fa8368e2715a89793267cbbe5cc7f5588
Ari Madian <akmadian@gmail.com>
2018-11-03 15:10:29 -0700

    Implement MongoDB write.
     - Implemented mongodb write function
     - Fixed HTML strings

---
+++ Deprecated/AlexaComp Client/DeviceLinkingForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+using Newtonsoft.Json;
+
+namespace AlexaComp {
+    public partial class DeviceLinkingForm : Form {
+
+        private static Request reqObj;
+        private static NetworkStream nwStream;
+
+        public DeviceLinkingForm() {
+            InitializeComponent();
+            this.ShowDialog();
+        }
+
+        public static void startDeviceLinking(object options) {
+            Console.WriteLine("Starting device linking form");
+            Options options_ = (Options)options;
+            reqObj = options_.req;
+            nwStream = options_.nwStream;
+            Application.Run(new DeviceLinkingForm());
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            string keyEntered = authKeyBox.Text;
+            string IPAddress = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+
+            if (keyEntered == reqObj.PRIMARY) {
+                Response res = new Response(true, "devicelinking", IPAddress, "");
+                res.sendResponse(nwStream);
+            } else {
+                Response res = new Response(false, "devicelinking", IPAddress, "");
+                res.sendResponse(nwStream);
+            }
+           
+
+            //AlexaCompSERVER.sendToLambda(json, fullmessage: true);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void authKeyBox_TextChanged(object sender, EventArgs e) {}
+    }
+}
--- Lambda/DeviceLinking_Lambda/index.js
+++ Lambda/DeviceLinking_Lambda/index.js
@@ -1,38 +1,39 @@
 var crypto = require('crypto');
 var config = require('./config.json');
-var doc = require('dynamodb-doc');
-var dynamodb = new doc.DynamoDB();
+const MongoClient = require('mongodb').MongoClient;
 
-console.log('v2')
+const URI_CONN_STRING = config.MONGODB.URI;
+
+function pushtoMDB(deviceID, userIP){
+    MongoClient.connect(URI_CONN_STRING, {useNewUrlParser: true}, function(err, client) {
+        if (err){
+            console.log(err);
+        } else {
+            console.log('No error during connection to MongoDB.')
+        }
+        const collection = client.db('IPTable').collection("IPTable");
+        var document = {deviceID:deviceID, userIP:userIP};
+        collection.insert(document, {w: 1}, function(err, records){
+            if (err){
+                console.log(err);
+            } else {
+                console.log('No error during write to MongoDB.');
+            }
+        });
+        console.log("Connected and wrote to MongoDB successfully.");
+        client.close();
+    });
+}
 
 function decrypt(text){
     if (text == undefined || text == null){
         return text;
     }
-  var decipher = crypto.createDecipher('aes-256-cbc',config.ENCRYPTION.KEY)
-  var dec = decipher.update(text,'hex','utf8')
-  dec += decipher.final('utf8');
-  return dec;
+    var decipher = crypto.createDecipher('aes-256-cbc',config.ENCRYPTION.KEY)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
 }
-
-/*
-function putToDDB(params){
-    console.log('Pushing to DDB')
-    var dynamodb = new doc.DynamoDB();
-    dynamodb.putItem({
-        TableName: "AlexaCompIPTable",
-        Item: {
-            Pair: {S: "testtesttest"},
-            UserIP: {S: "teststring123"},
-            testField:{S: "teststring456"}
-        }
-    },
-    function(err, data) {
-      console.log('In Update')
-      if (err) console.log(err);
-      else console.log(data);
-    });
-}*/
 
 exports.handler = async (event) => {
     console.log(event);
@@ -41,80 +42,44 @@ exports.handler = async (event) => {
     const deviceId = decrypt(queryParams['deviceId']);
     const userIp = event['requestContext']['identity']['sourceIp'];
 
+    console.log(event);
+
     if (key == undefined){
         console.log("No Password Provided In Request");
         const response = {
             statusCode: 403,
-            body: "<p>No password provided in request. <br/>\
-                      Please contact the maker of AlexaComp at akmadian@gmail.com.</p>"
+            body: "<p>No password provided in request. <br/>"\
+                     "Please contact the maker of AlexaComp at akmadian@gmail.com.</p>"
         };
         console.log('Response Defined')
         return response;
-    } else {
-        if (key != config.APIPASS){
-            console.log("Invalid Password");
-            const response = {
-                statusCode: 403,
-                body: "<p>Invalid password provided in request. <br/>\
-                          Please contact the maker of AlexaComp at akmadian@gmail.com.</p>"
-            };
-            console.log('Response Defined')
-            return response;
-        }
+    } else if (key != config.APIPASS){
+        console.log("Invalid Password");
+        const response = {
+            statusCode: 403,
+            body: "<p>Invalid password provided in request. <br/>"\
+                     "Please contact the maker of AlexaComp at akmadian@gmail.com.</p>"
+        };
+        console.log('Response Defined')
+        return response;
+    } else if (key == config.APIPASS){
+        pushtoMDB(deviceId, userIp);
+        var htmlv2 ="<h1>AlexaComp Device Linking</h1>"\
+                    "<p>Your devices have been linked! You may now close this tab.</p>"\
+                    "<p>IP - '+ userIp + '</p>"\
+                    "<p>Device ID - ' + deviceId + '</p>"\
+                    "<p>User ID   - ' + 'n/a' + '</p><br/>"\
+                    "<p>CRYPTED</p><br/>"\
+                    "<p>Crypted DeviceId - ' + queryParams['deviceId'] + '</p>"\
+                    "<p>Crypted UserId   - ' + 'n/a' +'</p>'"
+        console.log('HTML Defined')
+
+        const response = {
+            statusCode: 200,
+            body: event
+        };
+        console.log('Response Defined')
+        return response;
     }
-
-    var htmlv2 = '\
-                <h1>AlexaComp Device Linking</h1>\
-                <p>Your devices have been linked! You may now close this tab.</p>\
-                <p>IP - '+ userIp + '</p>\
-                <p>Device ID - ' + deviceId + '</p>\
-                <p>User ID   - ' + 'n/a' + '</p><br/>\
-                <p>CRYPTED</p><br/>\
-                <p>Crypted DeviceId - ' + queryParams['deviceId'] + '</p>\
-                <p>Crypted UserId   - ' + 'n/a' +'</p>'
-    console.log('HTML Defined')
-
-
-    var params = {
-        TableName: config.DYNAMODB.TABLENAME,
-        Key: {Pair: 'test123321'},
-        UpdateExpression: "SET #IpValue = :IP",
-        ExpressionAttributeNames: { "#IpValue" : "UserIp" },
-        ExpressionAttributeValues: { ":IP" :  'test456789'},
-        ReturnValues: "UPDATED_NEW"
-    };
-
-    /*
-    const params2 = {
-        TableName: "AlexaCompIPTable",
-        Item: {
-            "Pair": {"S": "testtesttest"},
-            "UserI": {"S": "teststring123"},
-            "testField":{"S": "teststring456"}
-        }
-    }*/
-
-    // putToDDB(params);
-
-    console.log('Pushing to DDB')
-    dynamodb.updateItem({
-        TableName: config.DYNAMODB.TABLENAME,
-        Item: {
-            "Pair": {"S": "testtesttest"},
-            "UserI": {"S": "teststring123"},
-            "testField":{"S": "teststring456"}
-        }
-    ,
-    function(err, data) {
-      console.log('In Update')
-      if (err) console.log(err);
-      else console.log(data);
-    });
-
-    const response = {
-        statusCode: 200,
-        body: htmlv2
-    };
-    console.log('Response Defined')
-    return response;
+    return event;
 };
