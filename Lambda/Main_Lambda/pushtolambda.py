4db5d11c1eb4c94a7ce7f7d39f6c36ae87fc3804
Ari Madian <akmadian@gmail.com>
2018-11-04 18:37:25 -0800

    Update pushtolambda.py

--- Lambda/Main_Lambda/pushtolambda.py
+++ Lambda/Main_Lambda/pushtolambda.py
@@ -1,19 +1,19 @@
 import subprocess
-import os
 import zipfile
 import time
+from os import path, walk
 from sys import argv
 
-
+base_path = path.os.path.dirname(path.realpath(argv[0]))
 
 def zipFiles():
     print('Zipping Files')
     filesToZip = ['index.js', 'Functions.js', 'package-lock.json', 'config.json']
     packageFile = zipfile.ZipFile("package.zip", "w")
 
-    for root, dirs, files in os.walk("node_modules/"):
+    for root, dirs, files in walk("node_modules/"):
         for file_ in files:
-            filesToZip.append(os.path.join(root, file_))
+            filesToZip.append(path.join(root, file_))
 
     for file in filesToZip:
         packageFile.write(file, compress_type=zipfile.ZIP_DEFLATED)
@@ -24,21 +24,20 @@ def zipFiles():
 
 def pushToLambda():
     print('Pushing To Lambda')
-    p = subprocess.Popen("aws lambda update-function-code --function-name ComputerCommands --zip-file fileb://" +
-                         "D:/Programming/1.GitHub_Repos/AlexaComp/Lambda/Main_Lambda/package.zip",
+    p = subprocess.Popen("aws lambda update-function-code --function-name ComputerCommands --zip-file fileb://" + base_path +"/package.zip",
                          stdout=subprocess.PIPE,
                          shell=True)
 
     (output, err) = p.communicate()
     p_status = p.wait()
     print("Command Output - \n", output.decode('utf-8'))
-    print("Command Exit Status/ Return Code - ", p_status)
+    print("Command Exit Status/ Return Code -", p_status, "OK" if p_status == 0 else "FAILED")
 
 
 startTime = time.time()
-if argv and '-ziponly' in argv[0]:
+if argv and '-ziponly' in argv:
     zipFiles()
 else:
     zipFiles()
     pushToLambda()
-print('Pushed to lambda in ' + str(time.time() - startTime)[:6] + ' seconds.')
+print('Completed in ' + str(time.time() - startTime)[:6] + ' seconds.')
