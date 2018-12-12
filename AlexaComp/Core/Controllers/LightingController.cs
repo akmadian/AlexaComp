6d1f12db392b4c89b45c7fe91f6e67825187d506
Ari Madian <akmadian@gmail.com>
2018-12-12 15:34:44 -0800

    Moved LightingController.cs's ColorMethods to separate file in Core/Helpers

--- AlexaComp/Core/Controllers/LightingController.cs
+++ AlexaComp/Core/Controllers/LightingController.cs
@@ -15,6 +15,7 @@ using RGB.NET.Devices.Novation;
 using RGB.NET.Devices.Razer;
 
 using AlexaComp.Core;
+using AlexaComp.Core.Helpers;
 
 namespace AlexaComp.Controllers {
 
@@ -205,8 +206,8 @@ namespace AlexaComp.Controllers {
                 ILedGroup ledGroup = new ListLedGroup(surface.Leds);
                 while (true) {
                     for (double j = 0; j < 1; j += hueStep) {
-                        RGBColor c = ColorMethods.HSLToRGB(j, 1.0, 0.5);
-                        ledGroup.Brush = new SolidColorBrush(new Color(c.R, c.G, c.B));
+                        Color c = (Color)ColorConversions.HSLToRGB(j, 1.0, 0.5);
+                        ledGroup.Brush = new SolidColorBrush(c);
                         Thread.Sleep(speed);
                     }
                 }
@@ -237,116 +238,6 @@ namespace AlexaComp.Controllers {
                 ledGroup.Brush = new SolidColorBrush(new Color(0, 0, 0));
             }
         }
-    }
-
-    /// <summary>
-    /// Methods that convert color values.
-    /// </summary>
-    class ColorMethods : AlexaCompCore {
-
-        /// <summary>
-        /// From: https://geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
-        /// This function converts HSL to RGB, I don't know anything else about it.
-        /// </summary>
-        /// <param name="h">Hue</param>
-        /// <param name="sl">Saturation</param>
-        /// <param name="l">Luminance</param>
-        /// <returns>An RGBColor object</returns>
-        public static RGBColor HSLToRGB(double h, double sl, double l) {
-            double v;
-            double r, g, b;
-
-            r = l;   // default to gray
-            g = l;
-            b = l;
-            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
-            if (v > 0) {
-                double m;
-                double sv;
-                int sextant;
-                double fract, vsf, mid1, mid2;
-
-                m = l + l - v;
-                sv = (v - m) / v;
-                h *= 6.0;
-                sextant = (int)h;
-                fract = h - sextant;
-                vsf = v * sv * fract;
-                mid1 = m + vsf;
-                mid2 = v - vsf;
-                switch (sextant) {
-                    case 0:
-                        r = v;
-                        g = mid1;
-                        b = m;
-                        break;
-                    case 1:
-                        r = mid2;
-                        g = v;
-                        b = m;
-                        break;
-                    case 2:
-                        r = m;
-                        g = v;
-                        b = mid1;
-                        break;
-                    case 3:
-                        r = m;
-                        g = mid2;
-                        b = v;
-                        break;
-                    case 4:
-                        r = mid1;
-                        g = m;
-                        b = v;
-                        break;
-                    case 5:
-                        r = v;
-                        g = m;
-                        b = mid2;
-                        break;
-                }
-            }
-            try {
-                return new RGBColor(Convert.ToByte(r * 255), Convert.ToByte(g * 255.0f), Convert.ToByte(b * 255.0f));
-            }
-            catch (OverflowException) {
-                Clog("Overflow Exception caught during attempt to return RGB object from HSLToRGB.");
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// Converts a hex value to an RGBColor object
-        /// Example hex string: FF00FF OR #FF00FF
-        /// </summary>
-        /// <param name="hex">A hex string</param>
-        /// <returns>An RGBColor object</returns>
-        public static RGBColor HexToRGB(string hex) {
-            string hexString = hex;
-            if (hexString.StartsWith("#")) { // Strip leading # if it exists
-                hexString = hexString.Substring(1);
-            }
-            string[] splitHex = hex.splitEveryN(2);
-            return new RGBColor(Convert.ToInt32(splitHex[0], 16), Convert.ToInt32(splitHex[1], 16), Convert.ToInt32(splitHex[2], 16));
-        }
-
-        /// <summary>
-        /// Converts an integer array with RGB values in it to an RGBColor object.
-        /// Example array: [123, 255, 65] 
-        /// </summary>
-        /// <param name="array">The integer array to convert</param>
-        /// <returns>An RGBColor object</returns>
-        public static RGBColor ArrToRGB(int[] array) => new RGBColor(array[0], array[1], array[2]);
-
-        /// <summary>
-        /// Converts an RGBColor object to a hex string
-        /// </summary>
-        /// <param name="color">The RGB object to convert</param>
-        /// <returns>A hex string</returns>
-        public static string RGBToHex(RGBColor color) => color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
-
-        public static int[] RGBToArr(RGBColor color) => new int[] { color.R, color.G, color.B };
     }
 
     class RGBColor {
---
+++ AlexaComp/Core/Helpers/ColorConversions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RGB.NET.Core;
+
+namespace AlexaComp.Core.Helpers {
+
+    class ColorConversions : AlexaCompCore{
+
+        /// <summary>
+        /// From: https://geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
+        /// This function converts HSL to RGB, I don't know anything else about it.
+        /// </summary>
+        /// <param name="h">Hue</param>
+        /// <param name="sl">Saturation</param>
+        /// <param name="l">Luminance</param>
+        /// <returns>An RGBColor object</returns>
+        public static Color? HSLToRGB(double h, double sl, double l) {
+            double v;
+            double r, g, b;
+
+            r = l;   // default to gray
+            g = l;
+            b = l;
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+            if (v > 0) {
+                double m;
+                double sv;
+                int sextant;
+                double fract, vsf, mid1, mid2;
+
+                m = l + l - v;
+                sv = (v - m) / v;
+                h *= 6.0;
+                sextant = (int)h;
+                fract = h - sextant;
+                vsf = v * sv * fract;
+                mid1 = m + vsf;
+                mid2 = v - vsf;
+                switch (sextant) {
+                    case 0:
+                        r = v;
+                        g = mid1;
+                        b = m;
+                        break;
+                    case 1:
+                        r = mid2;
+                        g = v;
+                        b = m;
+                        break;
+                    case 2:
+                        r = m;
+                        g = v;
+                        b = mid1;
+                        break;
+                    case 3:
+                        r = m;
+                        g = mid2;
+                        b = v;
+                        break;
+                    case 4:
+                        r = mid1;
+                        g = m;
+                        b = v;
+                        break;
+                    case 5:
+                        r = v;
+                        g = m;
+                        b = mid2;
+                        break;
+                }
+            }
+            try {
+                return new Color(Convert.ToByte(r * 255), Convert.ToByte(g * 255.0f), Convert.ToByte(b * 255.0f));
+            }
+            catch (OverflowException) {
+                Clog("Overflow Exception caught during attempt to return RGB object from HSLToRGB.");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Converts a hex value to an RGBColor object
+        /// Example hex string: FF00FF OR #FF00FF
+        /// </summary>
+        /// <param name="hex">A hex string</param>
+        /// <returns>An RGBColor object</returns>
+        public static Color HexToRGB(string hex) {
+            string hexString = hex;
+            if (hexString.StartsWith("#")) { // Strip leading # if it exists
+                hexString = hexString.Substring(1);
+            }
+            string[] splitHex = hex.splitEveryN(2);
+            return new Color(Convert.ToInt32(splitHex[0], 16), Convert.ToInt32(splitHex[1], 16), Convert.ToInt32(splitHex[2], 16));
+        }
+
+        /// <summary>
+        /// Converts an integer array with RGB values in it to an RGBColor object.
+        /// Example array: [123, 255, 65] 
+        /// </summary>
+        /// <param name="array">The integer array to convert</param>
+        /// <returns>An RGBColor object</returns>
+        public static Color ArrToRGB(int[] array) => new Color(array[0], array[1], array[2]);
+
+        /// <summary>
+        /// Converts an RGBColor object to a hex string
+        /// </summary>
+        /// <param name="color">The RGB object to convert</param>
+        /// <returns>A hex string</returns>
+        public static string RGBToHex(Color color) => color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+
+        public static int[] RGBToArr(Color color) => new int[] { color.R, color.G, color.B };
+    }
+}
